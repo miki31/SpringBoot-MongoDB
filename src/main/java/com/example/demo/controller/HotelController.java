4b20627de4c3a42dbc5835f15d452c4a05582b32
miki31 <it.misha2016@gmail.com>
2019-09-06 23:51:53 +0300

    GET methods in HotelController : getById , getByPricePernight

--- src/main/java/com/example/demo/controller/HotelController.java
+++ src/main/java/com/example/demo/controller/HotelController.java
@@ -5,6 +5,7 @@ import com.example.demo.repository.HotelRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/hotels")
@@ -23,11 +24,13 @@ public class HotelController {
         return hotels;
     }
 
+    //create new object in DB
     @PutMapping
     public void insert(@RequestBody Hotel hotel){
         this.hotelRepository.insert(hotel);
     }
 
+    //update object by ID
     @PostMapping
     public void update(@RequestBody Hotel hotel){
         this.hotelRepository.save(hotel);
@@ -36,5 +39,17 @@ public class HotelController {
     @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") String id){
         this.hotelRepository.deleteById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Hotel getById(@PathVariable("id") String id){
+        Optional<Hotel> hotel = this.hotelRepository.findById(id);
+        return hotel.get();
+    }
+
+    @GetMapping("/maxPrice/{maxPrice}")
+    public List<Hotel> getByPricePernight(@PathVariable("maxPrice") int maxPrice){
+        List<Hotel> hotels = this.hotelRepository.findByPricePerNightLessThan(maxPrice);
+        return hotels;
     }
 }
--- src/main/java/com/example/demo/repository/HotelRepository.java
+++ src/main/java/com/example/demo/repository/HotelRepository.java
@@ -4,6 +4,14 @@ import com.example.demo.model.Hotel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface HotelRepository extends MongoRepository<Hotel, String> {
+
+    Optional<Hotel> findById(String id);
+
+    List<Hotel> findByPricePerNightLessThan(int maxPrice);
+
 }
