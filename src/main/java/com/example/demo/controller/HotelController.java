60f0d036e2890e6c7fe804a6bcc246f697de68f3
miki31 <it.misha2016@gmail.com>
2019-09-06 22:52:45 +0300

    create HotelController method getAll

---
+++ src/main/java/com/example/demo/controller/HotelController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Hotel;
+import com.example.demo.repository.HotelRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hotels")
+public class HotelController {
+
+    private HotelRepository hotelRepository;
+
+    public HotelController(HotelRepository hotelRepository) {
+        this.hotelRepository = hotelRepository;
+    }
+
+    @GetMapping("/all")
+    public List<Hotel> getAll(){
+        List<Hotel> hotels = this.hotelRepository.findAll();
+
+        return hotels;
+    }
+}
